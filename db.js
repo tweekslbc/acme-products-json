75e8165295c653fbcf9ff5e72cfde0e2ada85806
pnbkn <pnblackburn@gmail.com>
2019-09-11 22:37:48 -0700

    Added create and destroy functionality

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
-const FILE = './products.JSON'
+const FILE = './products.json'
 
 module.exports = (FILE, validator) => {
-  const writeJSON = (data)=> {
-    return new Promise((resolve, reject)=> {
-      fs.writeFile(FILE, JSON.stringify(data, null, 2), (err)=> {
-        if(err){
+  const writeJSON = (data) => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(FILE, JSON.stringify(data, null, 2), (err) => {
+        if (err) {
           reject(err);
         }
         else {
@@ -15,14 +15,14 @@ module.exports = (FILE, validator) => {
     });
   };
 
-  const readJSON = ()=> {
-    return new Promise((resolve, reject)=> {
-      fs.readFile(FILE, (err, data)=> {
-        if(data){
+  const readJSON = () => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(FILE, (err, data) => {
+        if (data) {
           try {
             resolve(JSON.parse(data.toString()));
           }
-          catch(ex){
+          catch (ex) {
             reject(ex);
           }
         }
@@ -33,22 +33,22 @@ module.exports = (FILE, validator) => {
     });
   };
 
-  const destroy = (id)=> {
+  const destroy = (id) => {
     return findAll()
-      .then( items => {
-        return writeJSON(items.filter(item => item.id !== id*1));
+      .then(items => {
+        return writeJSON(items.filter(item => item.id !== id * 1));
       });
   };
 
-  const create = (item)=> {
+  const create = (item) => {
     return findAll()
-      .then( items => {
-        const error = validator(item, items);
-        if(error){
-          throw ({ message: error });
-        }
-        const maxId = items.reduce((acc, items)=> {
-          if(items.id > acc){
+      .then(items => {
+        // const error = validator(item, items);
+        // if (error) {
+        //   throw ({ message: error });
+        // }
+        const maxId = items.reduce((acc, items) => {
+          if (items.id > acc) {
             acc = items.id;
           }
           return acc;
@@ -57,12 +57,12 @@ module.exports = (FILE, validator) => {
         items.push(item);
         return writeJSON(items);
       })
-      .then(()=> {
+      .then(() => {
         return item;
       });
 
   };
-  const findAll = ()=> {
+  const findAll = () => {
     return readJSON();
   };
   return {
--- server.js
+++ server.js
@@ -1,35 +1,44 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const db= require('./db');
-
-const dataLayer = db('./products.json');
+const db = require('./db')('./products.json');
 
 app.use(express.json());
 
-app.get('/', (req, res, next)=> {
+app.get('/', (req, res, next) => {
   const it = path.join(__dirname, 'index.html');
 
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/api/products', async(req, res, next)=>{
+app.get('/api/products', async (req, res, next) => {
   try {
-  res.send(await dataLayer.findAll());
-}
-catch (ex){
-  next(ex);
-}
+    res.send(await db.findAll());
+  }
+  catch (ex) {
+    next(ex);
+  }
 });
 
-app.post('/api/products', async(req, res, next)=>{
+app.post('/api/products', async (req, res, next) => {
   console.log(req.body);
   try {
-  res.send(await dataLayer.create(req.body));
-}
-catch (ex){
-  next(ex);
-}
+    res.send(await db.create(req.body));
+  }
+  catch (ex) {
+    next(ex);
+  }
 });
 
-app.listen(7555, ()=> console.log('listening on port 7555'));
+app.delete('/api/products/:id', async (req, res, next) => {
+  console.log(req.body);
+  try {
+    await db.destroy(req.params.id);
+    res.sendStatus(204);
+  }
+  catch (ex) {
+    next(ex);
+  }
+});
+
+app.listen(7555, () => console.log('listening on port 7555'));
